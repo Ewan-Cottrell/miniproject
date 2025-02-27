d46614022665b9cf41fa55c9907eec0a8386185f
Ewan-Cottrell <ec1933@york.ac.uk>
2025-02-27 10:24:56 +0000

    Add files via upload

---
+++ countfingerprint.py
@@ -0,0 +1,35 @@
+# Libraries
+
+import rdkit
+import pandas as pd
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import AllChem as AChem
+
+# Smiles reading
+
+smiles_list = pd.read_csv('molecule_data.csv')
+smiles_list = smiles_list[['SMILES']]
+
+# Fingerprint parameter
+
+fpgen = AChem.GetMorganGenerator(radius=3, fpSize=4096, countSimulation=True) # Larger fpSize means less chance of colliding bits
+
+# Generates the fingerprints and array
+
+mol = [Chem.MolFromSmiles(smiles) for smiles in smiles_list['SMILES']]	
+fpx = [fpgen.GetFingerprint(mols) for mols in mol]
+arr = np.array(fpx)
+
+# Prints fingerprints and the array
+
+for i, fingerprint in enumerate(fpx):
+	print(f"Molecule {i+1} Fingerprint`: {fingerprint}")
+    
+print(arr)
+print(arr.shape)
+
+# Exports the array to a csv
+
+df = pd.DataFrame(arr)
+df.to_csv("count_array_fingerprint.csv", header=False, index=False)
